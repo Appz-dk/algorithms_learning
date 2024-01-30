780da235928de76a27bf66714d269e2504ba0927
Appz-dk <patrick.fyhn@gmail.com>
2024-01-31 00:29:03 +0100

    added binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  // Can only use binary search if the array to be searched is already sorted
+  // binary search can lead to off by one issues.
+  let startIndex = 0
+  let endIndex = haystack.length
+  
+  while (startIndex < endIndex) {
+    let middleIndex = Math.floor(startIndex + (endIndex - startIndex) / 2)
+    let currentValue = haystack[middleIndex]
 
+    if (currentValue === needle) {
+      startIndex = endIndex
+      return true
+    } else if (currentValue > needle) {
+      endIndex = middleIndex
+    } else {
+      startIndex = middleIndex + 1 // + 1 to take care of the "off by one issue"
+    }
+  }
+
+  return false
 }
